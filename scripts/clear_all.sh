3c55ce0963bf344f47c864161fe1b589d1d45f7f
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2024-11-28 15:30:41 +0000

    Allow specification of different dirs for different types

--- scripts/clear_all.sh
+++ scripts/clear_all.sh
@@ -9,7 +9,30 @@ if [ -z $TARGET_BASE_DIR ]; then
   TARGET_BASE_DIR=$DEFAULT_TARGET_BASE_DIR
 fi
 
-TARGET_DIR=$TARGET_BASE_DIR/css $SCRIPTS/clear_css.sh
-TARGET_DIR=$TARGET_BASE_DIR/img $SCRIPTS/clear_img.sh
-TARGET_DIR=$TARGET_BASE_DIR/html $SCRIPTS/clear_html.sh
-TARGET_DIR=$TARGET_BASE_DIR/js $SCRIPTS/clear_js.sh
+# The ennvar TARGET_CSS_DIR can be used to set the directory to contain css files
+if [ -z $TARGET_CSS_DIR ]; then
+  TARGET_CSS_DIR=$TARGET_BASE_DIR/css
+fi
+
+TARGET_DIR=$TARGET_CSS_DIR $SCRIPTS/clear_css.sh
+
+# The ennvar TARGET_IMG_DIR can be used to set the directory to contain image files
+if [ -z $TARGET_IMG_DIR ]; then
+  TARGET_IMG_DIR=$TARGET_BASE_DIR/img
+fi
+
+TARGET_DIR=$TARGET_IMG_DIR $SCRIPTS/clear_img.sh
+
+# The ennvar TARGET_HTML_DIR can be used to set the directory to contain html files
+if [ -z $TARGET_HTML_DIR ]; then
+  TARGET_HTML_DIR=$TARGET_BASE_DIR/html
+fi
+
+TARGET_DIR=$TARGET_HTML_DIR $SCRIPTS/clear_html.sh
+
+# The ennvar TARGET_JS_DIR can be used to set the directory to contain JS files
+if [ -z $TARGET_JS_DIR ]; then
+  TARGET_JS_DIR=$TARGET_BASE_DIR/js
+fi
+
+TARGET_DIR=$TARGET_JS_DIR $SCRIPTS/clear_js.sh
--- scripts/copy_all.sh
+++ scripts/copy_all.sh
@@ -9,7 +9,30 @@ if [ -z $TARGET_BASE_DIR ]; then
   TARGET_BASE_DIR=$DEFAULT_TARGET_BASE_DIR
 fi
 
-TARGET_DIR=$TARGET_BASE_DIR/css $SCRIPTS/copy_css.sh
-TARGET_DIR=$TARGET_BASE_DIR/img $SCRIPTS/copy_img.sh
-TARGET_DIR=$TARGET_BASE_DIR/html $SCRIPTS/copy_html.sh
-TARGET_DIR=$TARGET_BASE_DIR/js $SCRIPTS/copy_js.sh
+# The ennvar TARGET_CSS_DIR can be used to set the directory to contain css files
+if [ -z $TARGET_CSS_DIR ]; then
+  TARGET_CSS_DIR=$TARGET_BASE_DIR/css
+fi
+
+TARGET_DIR=$TARGET_CSS_DIR $SCRIPTS/copy_css.sh
+
+# The ennvar TARGET_IMG_DIR can be used to set the directory to contain image files
+if [ -z $TARGET_IMG_DIR ]; then
+  TARGET_IMG_DIR=$TARGET_BASE_DIR/img
+fi
+
+TARGET_DIR=$TARGET_IMG_DIR $SCRIPTS/copy_img.sh
+
+# The ennvar TARGET_HTML_DIR can be used to set the directory to contain html files
+if [ -z $TARGET_HTML_DIR ]; then
+  TARGET_HTML_DIR=$TARGET_BASE_DIR/html
+fi
+
+TARGET_DIR=$TARGET_HTML_DIR $SCRIPTS/copy_html.sh
+
+# The ennvar TARGET_JS_DIR can be used to set the directory to contain JS files
+if [ -z $TARGET_JS_DIR ]; then
+  TARGET_JS_DIR=$TARGET_BASE_DIR/js
+fi
+
+TARGET_DIR=$TARGET_JS_DIR $SCRIPTS/copy_js.sh
