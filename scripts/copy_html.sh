b1cb8d967652dc89ca8e0c721e76a73d1537eea7
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2024-11-28 16:14:27 +0000

    Add ability to replace brand link in copied header

--- scripts/copy_html.sh
+++ scripts/copy_html.sh
@@ -2,3 +2,19 @@
 
 SCRIPTS=$(dirname -- $(readlink -f $BASH_SOURCE))
 CONTENT_TYPE=html $SCRIPTS/copy_dir.sh
+
+DEFAULT_TARGET_DIR=$SCRIPTS/../../$CONTENT_TYPE
+
+# The ennvar TARGET_DIR can be used to set the target directory to copy files to
+if [ -z $TARGET_DIR ]; then
+  TARGET_DIR=$DEFAULT_TARGET_DIR
+fi
+
+# If envvars are specific to set specific locations within the copied html files, update the new ones in-place
+
+if [ ! -z $BRAND_LINK ]; then
+    # Replace the brand link in the common header with the desired value
+    ESCAPED_BRAND_LINK=$(printf '%s\n' "$BRAND_LINK" | sed -e 's/[\/&]/\\&/g')
+    REPLACE_CMD="sed -i -e 's/\$REPLACEME_BRAND_LINK/$ESCAPED_BRAND_LINK/' $TARGET_DIR/psdi-common-header.html"
+    eval $REPLACE_CMD
+fi
