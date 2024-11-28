db58287656009915223a3c98a5cf08946d2426a4
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2024-11-28 16:27:38 +0000

    Add ability to set specific header links and image location in header

--- scripts/copy_html.sh
+++ scripts/copy_html.sh
@@ -18,3 +18,17 @@ if [ ! -z $BRAND_LINK ]; then
     REPLACE_CMD="sed -i -e 's/\$REPLACEME_BRAND_LINK/$ESCAPED_BRAND_LINK/' $TARGET_DIR/psdi-common-header.html"
     eval $REPLACE_CMD
 fi
+
+if [ ! -z $HEADER_LINKS_SOURCE ]; then
+    # Replace the header links stub in the common header with the desired value
+    ESCAPED_HEADER_LINKS=$(printf '%s\n' "`cat $HEADER_LINKS_SOURCE`" | sed -e 's/[\/&]/\\&/g')
+    REPLACE_CMD="sed -i -e 's/\$REPLACEME_HEADER_LINKS/$ESCAPED_HEADER_LINKS/' $TARGET_DIR/psdi-common-header.html"
+    eval $REPLACE_CMD
+fi
+
+if [ ! -z $IMG_LOC ]; then
+    # Replace the image location stub in the common header with the desired value
+    ESCAPED_IMG_LOC=$(printf '%s\n' "$IMG_LOC" | sed -e 's/[\/&]/\\&/g')
+    REPLACE_CMD="sed -i -e 's/\$REPLACEME_IMG_LOC/$ESCAPED_IMG_LOC/g' $TARGET_DIR/psdi-common-header.html"
+    eval $REPLACE_CMD
+fi
