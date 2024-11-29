27b0a5d795c9b09d2a1a5c161231062b06eae29f
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2024-11-29 15:51:53 +0000

    Add option to only fetch assets and not copy them

--- fetch-common-style.sh
+++ fetch-common-style.sh
@@ -6,19 +6,27 @@ source $ROOTDIR/fetch-common-style.conf
 
 PACKAGE_FILENAME=$ROOTDIR/psdi-assets.tar.gz
 
-# If ASSET_VER is not set, download the latest release
-if [ -z $TARGET_DIR ]; then
-    # Code snippet courtesy of
-    # https://geraldonit.com/2019/01/15/how-to-download-the-latest-github-repo-release-via-command-line/
-    LOCATION=$(curl -s https://api.github.com/repos/psdi-uk/psdi-common-style/releases/latest \
-    | grep "tag_name" \
-    | awk '{print "https://github.com/psdi-uk/psdi-common-style/archive/" substr($2, 2, length($2)-3) ".tar.gz"}');
+# Check if the file already exists
+
+if [ -f $PACKAGE_FILENAME ]; then
+    echo "Assets are already available at $PACKAGE_FILENAME"
 else
-    LOCATION="https://github.com/psdi-uk/psdi-common-style/archive/"$ASSET_VER".tar.gz"
+    # If ASSET_VER is not set, download the latest release
+    if [ -z $TARGET_DIR ]; then
+        # Code snippet courtesy of
+        # https://geraldonit.com/2019/01/15/how-to-download-the-latest-github-repo-release-via-command-line/
+        LOCATION=$(curl -s https://api.github.com/repos/psdi-uk/psdi-common-style/releases/latest \
+        | grep "tag_name" \
+        | awk '{print "https://github.com/psdi-uk/psdi-common-style/archive/" substr($2, 2, length($2)-3) ".tar.gz"}');
+    else
+        LOCATION="https://github.com/psdi-uk/psdi-common-style/archive/"$ASSET_VER".tar.gz"
+    fi
+
+    echo "Downloading assets from $LOCATION"
+    curl -L -o $PACKAGE_FILENAME $LOCATION
 fi
 
-echo "Downloading assets from $LOCATION"
-curl -L -o $PACKAGE_FILENAME $LOCATION
+
 
 # Get the directory name within the archive
 ASSET_SUBDIR=`tar tf $PACKAGE_FILENAME | head -n 1`
@@ -28,5 +36,10 @@ echo "Extracting assets to $ASSET_DIR"
 cd $ROOTDIR
 tar xf $PACKAGE_FILENAME
 
-echo "Copying assets to configured locations"
-$ASSET_DIR/scripts/copy_all.sh
+if [ "$FETCH_ONLY" = "true" ]; then
+    echo "FETCH_ONLY is set to 'true', so assets won't be copied. They're available in the following directory:"
+    echo $ASSET_DIR
+else
+    echo "Copying assets to configured locations"
+    $ASSET_DIR/scripts/copy_all.sh
+fi
