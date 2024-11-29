d15a9e4bcdfea039591712f7586ca9dd50e6922f
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2024-11-29 15:42:34 +0000

    Add script to fetch assets

---
+++ fetch-common-style.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Source the configuration file to get settings in its envvars
+ROOTDIR=$(dirname -- $(readlink -f $BASH_SOURCE))
+source $ROOTDIR/fetch-common-style.conf
+
+PACKAGE_FILENAME=$ROOTDIR/psdi-assets.tar.gz
+
+# If ASSET_VER is not set, download the latest release
+if [ -z $TARGET_DIR ]; then
+    # Code snippet courtesy of
+    # https://geraldonit.com/2019/01/15/how-to-download-the-latest-github-repo-release-via-command-line/
+    LOCATION=$(curl -s https://api.github.com/repos/psdi-uk/psdi-common-style/releases/latest \
+    | grep "tag_name" \
+    | awk '{print "https://github.com/psdi-uk/psdi-common-style/archive/" substr($2, 2, length($2)-3) ".tar.gz"}');
+else
+    LOCATION="https://github.com/psdi-uk/psdi-common-style/archive/"$ASSET_VER".tar.gz"
+fi
+
+echo "Downloading assets from $LOCATION"
+curl -L -o $PACKAGE_FILENAME $LOCATION
+
+# Get the directory name within the archive
+ASSET_SUBDIR=`tar tf $PACKAGE_FILENAME | head -n 1`
+ASSET_DIR=$ROOTDIR/$ASSET_SUBDIR
+
+echo "Extracting assets to $ASSET_DIR"
+cd $ROOTDIR
+tar xf $PACKAGE_FILENAME
+
+echo "Copying assets to configured locations"
+$ASSET_DIR/scripts/copy_all.sh
