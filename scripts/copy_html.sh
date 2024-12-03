0cbb5d8ce88c890cdc707a68cce64de6d34b4f4f
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2024-12-03 10:16:44 +0000

    Replace image locations in common footer too

--- scripts/copy_html.sh
+++ scripts/copy_html.sh
@@ -33,9 +33,10 @@ if [ ! -z $HEADER_LINKS_SOURCE ]; then
 fi
 
 if [ ! -z $IMG_LOC ]; then
-    # Replace the image location stub in the common header with the desired value
+    # Replace the image location stub in the common header and footer with the desired value
     LINE_TO_REPACE_ESCAPED='https:\/\/psdi-uk.github.io\/css-template\/images\/'
     ESCAPED_IMG_LOC=$(printf '%s\n' "$IMG_LOC" | sed -e 's/[\/&]/\\&/g')
-    REPLACE_CMD="sed -i -e 's/$LINE_TO_REPACE_ESCAPED/$ESCAPED_IMG_LOC\//g' $TARGET_DIR/psdi-common-header.html"
-    eval $REPLACE_CMD
+    REPLACE_CMD_HEAD="sed -i -e 's/$LINE_TO_REPACE_ESCAPED/$ESCAPED_IMG_LOC\//g' $TARGET_DIR"
+    eval $REPLACE_CMD_HEAD/psdi-common-header.html
+    eval $REPLACE_CMD_HEAD/psdi-common-footer.html
 fi
