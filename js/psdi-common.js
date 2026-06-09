7fc5381a294be0f49b3d0aef0fda866cf0c46c1d
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2026-06-09 16:13:20 +0100

    feat(misc): Add light/dark mode button disconnect function

--- js/psdi-common.js
+++ js/psdi-common.js
@@ -8,8 +8,8 @@ const ORIG_TITLE_LINK = "./"
 const DEFAULT_TITLE = "";
 const DEFAULT_TITLE_LINK_TARGET = "./";
 const DEFAULT_HEADER_LINKS_SOURCE = "./header-links.html";
-const DEFAULT_HEADER_SOURCE = "https://psdi-uk.github.io/psdi-common-style/html/psdi-common-header.html";
-const DEFAULT_FOOTER_SOURCE = "https://psdi-uk.github.io/psdi-common-style/html/psdi-common-footer.html";
+const DEFAULT_HEADER_SOURCE = "./psdi-common-header.html";
+const DEFAULT_FOOTER_SOURCE = "./psdi-common-footer.html";
 
 let title = DEFAULT_TITLE;
 let useDefaultTitle = true;
@@ -90,6 +90,14 @@ export function connectModeToggleButton() {
   const lModeToggleButton = document.querySelectorAll(".color-mode-toggle");
   lModeToggleButton.forEach(function (modeToggleButton) {
     modeToggleButton.addEventListener("click", toggleMode);
+  });
+}
+
+export function disconnectModeToggleButton() {
+  // Disconnect the mode toggle function from the button
+  const lModeToggleButton = document.querySelectorAll(".color-mode-toggle");
+  lModeToggleButton.forEach(function (modeToggleButton) {
+    modeToggleButton.removeEventListener("click", toggleMode);
   });
 }
 
